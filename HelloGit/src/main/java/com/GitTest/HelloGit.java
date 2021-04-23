338c3485df21de4cb348586122a0663770768581
{Th'Dice} <{1967628870@qq.com}>
2021-04-23 11:30:46 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/GitTest/HelloGit.java
@@ -0,0 +1,14 @@
+package com.GitTest;
+
+/**
+ * @ClassName HelloGit
+ * @Description TODO
+ * @Author 19676
+ * @Data 11:19
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
